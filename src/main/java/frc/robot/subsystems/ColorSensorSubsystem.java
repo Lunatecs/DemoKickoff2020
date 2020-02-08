da030ff34ff4d4382a8d4b7f2c0ea1deb2a61827
baxtrax <bradofficialbox@gmail.com>
2020-02-08 11:25:40 -0500

    Added cool color wdiget for color sensor

--- src/main/java/frc/robot/subsystems/ColorSensorSubsystem.java
+++ src/main/java/frc/robot/subsystems/ColorSensorSubsystem.java
@@ -6,14 +6,18 @@
 /*----------------------------------------------------------------------------*/
 
 package frc.robot.subsystems;
+
 import com.revrobotics.ColorSensorV3;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj.I2C;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.SuppliedValueWidget;
 
 import com.revrobotics.ColorMatchResult;
 import com.revrobotics.ColorMatch;
 import edu.wpi.first.wpilibj.util.Color;
+import java.util.Map;
 
 
 public class ColorSensorSubsystem extends SubsystemBase {
@@ -29,17 +33,32 @@ public class ColorSensorSubsystem extends SubsystemBase {
   private final Color kBlueTarget = ColorMatch.makeColor(0.143, 0.427, 0.429);
   private final Color kGreenTarget = ColorMatch.makeColor(0.197, 0.561, 0.240);
   private final Color kRedTarget = ColorMatch.makeColor(0.561, 0.232, 0.114);
-  private final Color kYellowTarget = ColorMatch.makeColor(0.361, 0.524, 0.113);
+  private final Color kYellowTarget = ColorMatch.makeColor(0.361, 0.524, 0.113);  
 
- 
-  
-
+  private SuppliedValueWidget colorWidget = 
+    Shuffleboard.getTab("Colors").addBoolean("WheelColor", () -> true);
 
   public ColorSensorSubsystem() {
     colorMatcher.addColorMatch(kBlueTarget);
     colorMatcher.addColorMatch(kGreenTarget);
     colorMatcher.addColorMatch(kRedTarget);
     colorMatcher.addColorMatch(kYellowTarget);
+
+    Shuffleboard.getTab("Colors")
+    .addBoolean("isBlue", () -> colorMatcher.matchClosestColor(colorSensor.getColor()).color == kBlueTarget)
+    .withProperties(Map.of("colorWhenTrue", "blue", "colorWhenFalse","black"));
+
+    Shuffleboard.getTab("Colors")
+    .addBoolean("isRed", () -> colorMatcher.matchClosestColor(colorSensor.getColor()).color  == kRedTarget)
+    .withProperties(Map.of("colorWhenTrue", "red", "colorWhenFalse","black"));
+
+    Shuffleboard.getTab("Colors")
+    .addBoolean("isGreen", () -> colorMatcher.matchClosestColor(colorSensor.getColor()).color  == kGreenTarget)
+    .withProperties(Map.of("colorWhenTrue", "green"));
+
+    Shuffleboard.getTab("Colors")
+    .addBoolean("isYellow", () -> colorMatcher.matchClosestColor(colorSensor.getColor()).color  == kYellowTarget)
+    .withProperties(Map.of("colorWhenTrue", "yellow"));
   }
 
   public int getBlue() {
@@ -60,7 +79,6 @@ public class ColorSensorSubsystem extends SubsystemBase {
     SmartDashboard.putNumber("Red Value", this.getRed());
     SmartDashboard.putNumber("Blue Value", this.getBlue());
     SmartDashboard.putNumber("Green Value", this.getGreen());
-
   
     Color colorDetected = colorSensor.getColor();
   
@@ -69,21 +87,28 @@ public class ColorSensorSubsystem extends SubsystemBase {
   
     if (match.color == kBlueTarget) {
       colorString = "Blue";
+      colorWidget.withProperties(Map.of("Color when true", "blue"));
     } else if (match.color == kRedTarget) {
       colorString = "Red";
+      colorWidget.withProperties(Map.of("Color when true", "red"));
     } else if (match.color == kGreenTarget) {
       colorString = "Green";
+      colorWidget.withProperties(Map.of("Color when true", "green"));
     } else if (match.color == kYellowTarget) {
       colorString = "Yellow";
+      colorWidget.withProperties(Map.of("Color when true", "yellow"));
     } else {
       colorString = "Unknown";
+      colorWidget.withProperties(Map.of("Color when true", "black"));
     }
 
-  SmartDashboard.putNumber("Red", colorDetected.red);
-  SmartDashboard.putNumber("Green", colorDetected.green);
-  SmartDashboard.putNumber("Blue", colorDetected.blue);
-  SmartDashboard.putNumber("Confidence", match.confidence);
-  SmartDashboard.putString("Detected Color", colorString);
+
+
+    SmartDashboard.putNumber("Red", colorDetected.red);
+    SmartDashboard.putNumber("Green", colorDetected.green);
+    SmartDashboard.putNumber("Blue", colorDetected.blue);
+    SmartDashboard.putNumber("Confidence", match.confidence);
+    SmartDashboard.putString("Detected Color", colorString);
 
   }
 }
